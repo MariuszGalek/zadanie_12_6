044937262aaf95c2d46e7fb12e48014991bda3c3
Mariusz Galek <mgalek1@gmail.com>
2018-01-23 22:09:42 +0100

    Add js code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+var url = 'https://restcountries.eu/rest/v2/name/';
+var countriesList = $('#countries');
 
+$('#search').click(searchCountries);
+
+function searchCountries() {
+  	var countryName = $('#country-name').val();
+  	if(!countryName.length) countryName = 'Poland';
+  	$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
+
+function showCountriesList(resp) {
+  	countriesList.empty();
+  	resp.forEach(function(item) {
+   		$('<li>').addClass('country').append(item.name).appendTo(countriesList);
+      $('<li>').append('<span>Capital</span>' + ":  " + item.capital).appendTo(countriesList);
+      $('<li>').append('<span>Area (km&sup2;)</span>' + ":  " + item.area).appendTo(countriesList);
+      $('<li>').append('<span>Population</span>' + ":  " + item.population).appendTo(countriesList);
+      $('<li>').append('<span>Currency</span>' + ":  " + item.currencies[0].name).appendTo(countriesList);
+      $('<li>').addClass('bottom-line').appendTo(countriesList);
+	});
+}
